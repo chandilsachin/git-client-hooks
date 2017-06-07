0408aff3a297f6a01308f9a16a36463959905048
BBI-M1025 <sachin.chandil@media4u.com>
2017-06-07 10:47:18 +0530

    Improved script to write hook files

--- install-git-hook.sh
+++ install-git-hook.sh
@@ -3,15 +3,56 @@
 mkdir -p tests tests/hooks tests/non-ui tests/ui
 
 # Write test hook
+cat > tests/hooks/non-ui-test-hook << EOM
+#Colors
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
-echo 'mocha $(find "tests/non-ui" -name "*.js")' > tests/hooks/non-ui-test-hook
+# Runing test cases
+res=\$(script -q /dev/null mocha \$(find "tests/non-ui" -name "*.js"))
+RESULT=\$?
+[ \$RESULT -ne 0 ] && echo -e "\$res \${RED}Some TestCaes did not pass. Please check your code.\${NC}" && exit 1
+echo "All test cases passed.\n"
+exit 0
+EOM
 chmod +x tests/hooks/non-ui-test-hook
 
-echo 'mocha $(find "tests/ui" -name "*.js")' > tests/hooks/ui-test-hook
+cat > tests/hooks/ui-test-hook << EOM
+#Colors
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Runing test cases
+res=\$(script -q /dev/null mocha \$(find "tests/ui" -name "*.js"))
+RESULT=\$?
+[ \$RESULT -ne 0 ] && echo -e "\$res \${RED}Some TestCaes did not pass. Please check your code.\${NC}" && exit 1
+echo "All test cases passed.\n"
+exit 0
+EOM
 chmod +x tests/hooks/ui-test-hook
 
 touch tests/hooks/post-pull
 chmod +x tests/hooks/post-pull
+
+# ========
+#Write a sample test case
+cat > tests/non-ui/sample-test.js << EOM
+var assert = require('assert');
+
+describe("All element creation test:", function(){
+
+    before(function () {
+
+    });
+
+    after(function () {
+
+    });
+    it("Label", function(){
+        assert.equal(false, true)
+    });
+});
+EOM
 
 # ========
 # Check if pre-commit file exists
@@ -32,30 +73,26 @@ fi
 
 # write code to pre-commit file
 cat > .git/hooks/pre-commit << EOM
-#Colors
-RED='\033[0;31m'
-NC='\033[0m' # No Color
 
 # Javascript unit tests 
-res=$(script -q /dev/null ./tests/hooks/non-ui-test-hook )
-RESULT=$?
-[ $RESULT -ne 0 ] && echo -e "$res" && exit 1
-echo -e "All test cases passed.\n"
+res=\$(script -q /dev/null ./tests/hooks/non-ui-test-hook)
+RESULT=\$?
+echo \$res
+[ \$RESULT -ne 0 ] && exit 1
 exit 0
 EOM
 
 chmod +x .git/hooks/pre-commit
 
 cat > .git/hooks/post-update << EOM
-#Colors
-RED='\033[0;31m'
-NC='\033[0m' # No Color
 
 # Javascript unit tests 
-res=$(script -q /dev/null ./tests/hooks/post-pull)
-RESULT=$?
-[ $RESULT -ne 0 ] && echo -e "$res" && exit 1
-echo -e "All test cases passed.\n"
+res=\$(script -q /dev/null ./tests/hooks/post-pull)
+RESULT=\$?
+echo \$res
+[ \$RESULT -ne 0 ] && exit 1
 exit 0
 EOM
 chmod +x .git/hooks/post-update
+
+echo "Hooks installed successfully."
