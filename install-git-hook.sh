a467b80e7b081acc932a102428fd6832d475eba7
BBI-M1025 <sachin.chandil@media4u.com>
2017-06-08 19:36:43 +0530

    Output of user hook files are not properly formated - solved

--- install-git-hook.sh
+++ install-git-hook.sh
@@ -4,6 +4,7 @@ mkdir -p tests tests/hooks tests/non-ui tests/ui
 
 # Write test hook
 cat > tests/hooks/non-ui-test-hook << EOM
+#!/bin/bash
 #Colors
 RED='\033[0;31m'
 NC='\033[0m' # No Color
@@ -11,13 +12,14 @@ NC='\033[0m' # No Color
 # Runing test cases
 res=\$(script -q /dev/null mocha \$(find "tests/non-ui" -name "*.js"))
 RESULT=\$?
-[ \$RESULT -ne 0 ] && echo -e "\$res \${RED}Some TestCaes did not pass. Please check your code.\${NC}" && exit 1
-echo "All test cases passed.\n"
+[ \$RESULT -ne 0 ] && echo "\$res \${RED}Some Test cases failed, please check your code.\${NC}\n" && exit 1
+echo -e "All test cases passed.\n"
 exit 0
 EOM
 chmod +x tests/hooks/non-ui-test-hook
 
 cat > tests/hooks/ui-test-hook << EOM
+#!/bin/bash
 #Colors
 RED='\033[0;31m'
 NC='\033[0m' # No Color
@@ -25,8 +27,8 @@ NC='\033[0m' # No Color
 # Runing test cases
 res=\$(script -q /dev/null mocha \$(find "tests/ui" -name "*.js"))
 RESULT=\$?
-[ \$RESULT -ne 0 ] && echo -e "\$res \${RED}Some TestCaes did not pass. Please check your code.\${NC}" && exit 1
-echo "All test cases passed.\n"
+[ \$RESULT -ne 0 ] && echo "\$res \${RED}Some Test cases failed, please check your code.\${NC}\n" && exit 1
+echo -e "All test cases passed.\n"
 exit 0
 EOM
 chmod +x tests/hooks/ui-test-hook
@@ -73,24 +75,22 @@ fi
 
 # write code to pre-commit file
 cat > .git/hooks/pre-commit << EOM
-
-# Javascript unit tests 
-res=\$(script -q /dev/null ./tests/hooks/non-ui-test-hook)
+#!/bin/bash
+# Javascript unit tests
+res="\$(script -q /dev/null ./tests/hooks/non-ui-test-hook)"
 RESULT=\$?
-echo \$res
-[ \$RESULT -ne 0 ] && exit 1
+[ \$RESULT -ne 0 ] && echo "\$res" && exit 1
 exit 0
 EOM
 
 chmod +x .git/hooks/pre-commit
 
 cat > .git/hooks/post-update << EOM
-
-# Javascript unit tests 
-res=\$(script -q /dev/null ./tests/hooks/post-pull)
+#!/bin/bash
+# Javascript unit tests
+res="\$(script -q /dev/null ./tests/hooks/post-pull)"
 RESULT=\$?
-echo \$res
-[ \$RESULT -ne 0 ] && exit 1
+[ \$RESULT -ne 0 ] && echo "\$res" && exit 1
 exit 0
 EOM
 chmod +x .git/hooks/post-update
